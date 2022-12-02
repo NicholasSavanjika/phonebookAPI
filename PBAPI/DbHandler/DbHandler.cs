02d32601fa2685e341cfda2d1eb12db65e9f67f2
NicholasSavanjika <100638907@student.kangan.edu.au>
2022-12-02 15:06:23 +1100

    anh helped fix dbhandler with async

--- PBAPI/DbHandler/DbHandler.cs
+++ PBAPI/DbHandler/DbHandler.cs
@@ -9,22 +9,32 @@ namespace PBAPI.DbHandler
             "Username=elsa;" +
             "Password=kangan;" +
             "Database=elsa_phonebook";
-        private NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING);
+        private NpgsqlConnection connection;
 
-        public List<Contact> GetAllContacts() {
+        public DbHandler() {
+            this.connection = new NpgsqlConnection(CONNECTION_STRING);
+            
             // start connection
-            this.connection.Open();
+            this.connection.OpenAsync(); 
+        }
+        
+        public async Task<List<Contact>> GetAllContacts() {
+
 
             // set up a query
             NpgsqlCommand command = new NpgsqlCommand("SELECT person_name.id, CONCAT(first_name , ' ' , last_name) AS name, contact_number FROM person_name, phone_book WHERE person_name.id=phone_book.id;", this.connection);
-
-            // execute the query
-            NpgsqlDataReader reader = command.ExecuteReader();
             var contacts = new List<Contact>();
 
-            while(reader.Read()) {
-                System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
-                contacts.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
+            // execute the query
+
+
+            using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
+                    contacts.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
+                }
             }
 
             return contacts;
