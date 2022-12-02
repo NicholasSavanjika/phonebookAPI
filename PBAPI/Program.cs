f9c5053847a9d56869eb7374aaca79aa9c35fd79
NicholasSavanjika <100638907@student.kangan.edu.au>
2022-12-02 11:33:12 +1100

    added cors

--- PBAPI/Program.cs
+++ PBAPI/Program.cs
@@ -1,7 +1,16 @@
 using PBAPI.DbHandler;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(b =>{
+    b.AllowAnyOrigin();
+    b.AllowAnyMethod();
+    b.AllowAnyHeader();
+});
 
 var dbHandler = new DbHandler();
 //Phonebook phonebook = new Phonebook();
