0a13bab4890194152ff139b1c68c3f518f14acc1
NicholasSavanjika <100638907@student.kangan.edu.au>
2023-03-07 10:14:12 +1100

    commenting on code

--- PBAPI/DbHandler/DbHandler.cs
+++ PBAPI/DbHandler/DbHandler.cs
@@ -5,6 +5,7 @@ namespace PBAPI.DbHandler
 {
     public class DbHandler
     {
+        // Connect to the database and login
         private const string CONNECTION_STRING = "Host=db.wifiandinternetsolutions.com.au;" +
             "Username=dtpphoneuser;" +
             "Password=kangan;" +
@@ -17,15 +18,15 @@ namespace PBAPI.DbHandler
             // start connection
             this.connection.OpenAsync(); 
         }
-        //Get all contacts
+        // Get all contacts
         public async Task<List<Contact>> GetAllContacts() {
 
 
-            // set up a query
+            // set up a query to get all contacts
             string query = "SELECT person_name.id, CONCAT(first_name , ' ' , last_name) AS name, contact_number FROM person_name, phone_book WHERE person_name.id=phone_book.id;";
             var contacts = new List<Contact>();
 
-            // execute the query
+            // execute the query to get all contacts
             try {
                 await using var dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
                 await using var command = dataSource.CreateCommand(query);
@@ -35,21 +36,7 @@ namespace PBAPI.DbHandler
                     System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
                     contacts.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
                 }
-                /*if (this.connection.State == System.Data.ConnectionState.Closed) {
-                    await this.connection.OpenAsync();
-                }
-                NpgsqlCommand command = new NpgsqlCommand(query, this.connection);
-
-                using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
-                        contacts.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
-                    }
-                }*/
-
-                
+            // prevent API from crashing if error occurs
             } catch (Exception e) {
                 contacts = new List<Contact>();
                 System.Console.WriteLine(e.Message);
@@ -60,15 +47,16 @@ namespace PBAPI.DbHandler
             }
             return contacts;            
         }
-        //Get one contact (or many contacts if I coded it that way)
+
+        // Search for contacts in database
         public async Task<List<Contact>> GetContact(string searchTerm) {
 
-            // set up a query
+            // set up a query to search for contacts
             string query2 = "SELECT * FROM (SELECT person_name.id, CONCAT(first_name , ' ' , last_name) AS name, contact_number FROM person_name, phone_book WHERE person_name.id=phone_book.id) AS nameOutput WHERE name ILIKE @p1;";
             searchTerm = $"%{searchTerm}%";
             var contact = new List<Contact>();
             
-            // execute the query
+            // execute the query to search for contacts
             try {
                 await using var dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
                 await using var command = dataSource.CreateCommand(query2);
@@ -81,26 +69,7 @@ namespace PBAPI.DbHandler
                     System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
                     contact.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
                 }
-                /*if (this.connection.State == System.Data.ConnectionState.Closed) {
-                    await this.connection.OpenAsync();
-                }
-                NpgsqlCommand command = new NpgsqlCommand(query2, this.connection)
-                {
-                    Parameters =
-                    {
-                        new("p1", searchTerm)
-                    }
-                };
-                using (NpgsqlDataReader reader = await command.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        System.Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
-                        contact.Add(new Contact(reader[0].ToString(), reader[1].ToString(), reader[2].ToString()));
-                    }
-                }*/
-
-                
+            // prevent API from crashing if error occurs    
             } catch (Exception e) {
                 contact = new List<Contact>();
                 System.Console.WriteLine(e.Message);
@@ -111,12 +80,10 @@ namespace PBAPI.DbHandler
             }
             return contact;
         }
-        //Add new contact
+
+        // Add new contact
         public async Task<Contact?> AddContact(Contact newContact) {
             
-            /*if (newContact.Name.All(Char.IsDigit) == false){
-                return null;
-            }*/
             int contactNumberCheck;
             if (int.TryParse(newContact.ContactNumber, out contactNumberCheck) == false) {
                 return null;
@@ -126,10 +93,11 @@ namespace PBAPI.DbHandler
             }
             string firstName = null;
             string lastName = null;
+            // check if contact has a last name
             if (newContact.Name.Contains(" ")) {
 
             
-                // Location of the space
+                // Get position of the space
                 int index = newContact.Name.IndexOf(" ");
             
                 // Get first name
@@ -142,7 +110,7 @@ namespace PBAPI.DbHandler
                 firstName = newContact.Name;
             }
 
-            // set up a query
+            // set up a query to add a contact
             string query3 = "WITH add_person AS (INSERT INTO person_name (first_name, last_name) VALUES (@p1, @p2) RETURNING ID) INSERT INTO phone_book (id, contact_type, contact_number) SELECT id, 'Mobile', @p3 FROM add_person;";
             firstName = $"{firstName}";
             lastName = $"{lastName}";
@@ -150,7 +118,7 @@ namespace PBAPI.DbHandler
             var addContact = new Contact();
             
 
-            // execute the query
+            // execute the query to add a contact
             try {
                 await using var dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
                 await using var command = dataSource.CreateCommand(query3);
@@ -161,23 +129,8 @@ namespace PBAPI.DbHandler
                 var result = await command.ExecuteNonQueryAsync();
                 System.Console.WriteLine(result);
                 
-                // if (this.connection.State == System.Data.ConnectionState.Closed) {
-                //     await this.connection.OpenAsync();
-                // }
-                // NpgsqlCommand command = new NpgsqlCommand(query3, this.connection)
-                // {
-                //     Parameters =
-                //     {
-                //         new("p1", firstName),
-                //         new("p2", lastName),
-                //         new("p3", newContact.ContactNumber)
-                //     }
-                // };
-
-                //var result = command.ExecuteNonQuery();
-
-                //addContact.Name = firstName;
                 return newContact;
+            // prevent API from crashing if error occurs  
             } catch (Exception e) {
                 newContact = new Contact();
                 System.Console.WriteLine(e.Message);
@@ -186,17 +139,17 @@ namespace PBAPI.DbHandler
             return newContact;
         
         }
-        //Delete existing contact
+        // Delete existing contact
         public async Task<Contact?> DeleteContact(int id) {
 
 
-            // set up a query
+            // set up a query to delete a contact
             string query4 = "WITH delete_person AS (DELETE FROM person_name WHERE person_name.id = @p1 RETURNING ID ) DELETE FROM phone_book WHERE phone_book.id IN (SELECT ID FROM delete_person)";
 
             var deleteContact = new Contact();
             
 
-            // execute the query
+            // execute the query to delete a contact
             try {
                 await using var dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
                 await using var command = dataSource.CreateCommand(query4);
@@ -210,7 +163,7 @@ namespace PBAPI.DbHandler
                 }
                 return deleteContact;
 
-
+            // prevent API from crashing if error occurs  
             } catch (Exception e) {
                 deleteContact = new Contact();
                 System.Console.WriteLine(e.Message);
@@ -218,9 +171,10 @@ namespace PBAPI.DbHandler
             return deleteContact;
         
         }
-        //Update existing contact
+        // Update existing contact
         public async Task<Contact?> UpdateContact(Contact updateContact) {
             
+            //Check for id to update, and for name and contact number to update in database
             if(updateContact.Id == "" || updateContact.Id == null) {
                 return null;
             }
@@ -235,10 +189,11 @@ namespace PBAPI.DbHandler
             Int32.TryParse(updateContact.Id, out updateId);
             string updateFirstName = null;
             string updateLastName = null;
+            // check if contact has a last name
             if (updateContact.Name.Contains(" ")) {
 
             
-                // Location of the space
+                // Get position of the space
                 int index = updateContact.Name.IndexOf(" ");
             
                 // Get first name
@@ -251,7 +206,7 @@ namespace PBAPI.DbHandler
                 updateFirstName = updateContact.Name;
             }
 
-            // set up a query
+            // set up a query to update a contact
             string query5 = "WITH upd1 AS (	UPDATE person_name SET first_name= @p1, last_name= @p2 WHERE id= @p4 returning id) UPDATE phone_book SET contact_number= @p3 FROM upd1 WHERE upd1.id=phone_book.id;";
             
             updateFirstName = $"{updateFirstName}";
@@ -260,7 +215,7 @@ namespace PBAPI.DbHandler
 
             
 
-            // execute the query
+            // execute the query to update a contact
             try {
                 await using var dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
                 await using var command = dataSource.CreateCommand(query5);
@@ -273,6 +228,7 @@ namespace PBAPI.DbHandler
                 System.Console.WriteLine(result);
                 
                 return updateContact;
+            // prevent API from crashing if error occurs  
             } catch (Exception e) {
                 updateContact = new Contact();
                 System.Console.WriteLine(e.Message);
